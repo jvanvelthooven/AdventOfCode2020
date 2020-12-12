a24eab1b3b0223be6ac92609445f506cccb8e0d0
Jeroen van Velthooven <jeroen.van.velthooven@vanderlande.com>
2020-12-12 13:32:41 +0100

    readability changed

--- puzzle11solution.ps1
+++ puzzle11solution.ps1
@@ -1,18 +1,18 @@
-$content = get-content .\puzzle11input.txt
-$cols = $content.count
-$rows = $content[0].length
-$grid = New-Object 'object[,]' $content.count,$content[0].length
-
 Function Update-Seats {
   Param (
     $seats
   )
 
+  # set columns
   $cols = $seats.count
+  # set rows
   $rows = $seats[0].length
+  # create matching grid
   $grid = New-Object 'object[,]' $seats.count,$seats[0].length
+  # create object for 8 neighboars
   $neighbours = (-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)
   
+  # update grid to match input
   $counter1 = 0
   Foreach ($line in $seats) {
     $line = $line.ToCharArray()
@@ -34,53 +34,71 @@ Function Update-Seats {
     $counter1++
   }
 
+  # run through all seats and update output with rules. Want to keep it visual so setting output to lines
   for ($col = 0;$col -lt $cols;$col++) {
     for ($row = 0;$row -lt $rows;$row++) {
       Switch ($grid[$col,$row]) {
+        # no change, update line
         '.' {
           $showLine += '.'
         }
+        # based on empty chair rules
         'L' {
+          # read filled neighboars
           $nb = $neighbours | Where-Object {
             If (($col + $_[0]) -ge 0 -and ($row + $_[1]) -ge 0) {
               $grid[($col + $_[0]),($row + $_[1])] -eq '#'
             }
           }
+          # no filled neighboars ,occupy seat 
           If (!$nb) {
             $showLine += '#'
             $seatsChanged = $true
           }
+          # neighbour found, keep empty
           Else {
             $showLine += 'L'
           }
         }
+        # based on filled chair rules
         '#' {
+          # read filled neighboars
           $nb = $neighbours | Where-Object {
             If (($col + $_[0]) -ge 0 -and ($row + $_[1]) -ge 0) {
               $grid[($col + $_[0]),($row + $_[1])] -eq '#'
             }
           }
+          # neighboars above threshold, change to empty
           If ($nb.count -ge 4) {
             $showLine += 'L'
             $seatsChanged = $true
           }
+          # below threshold can keep chair filled
           Else {
             $showLine += '#'
           }
         }
       }
     }
+    # add to multiline output
     $showAll += ,$showLine
+    # clean variable
     remove-variable showLine
   }
+  # show output if we changed
   If ($seatsChanged) {
     $showAll
   }
+  # no change, count number of filled chairs
   Else {
     (($showAll -join '').ToCharArray() | Where-Object {$_ -eq '#'}).count
   }
 }
-While ($next -isnot [int]){$next = Update-Seats -seats $next};$next
+# initial from input
+$next = Update-Seats -seats (get-content .\puzzle11input.txt)
+# loop through changes and report number of chairs
+While ($next -isnot [int]){$next = Update-Seats -seats $next}
+$next
 
 '2'
 Function Update-Seats2 {
@@ -88,11 +106,16 @@ Function Update-Seats2 {
     $seats
   )
 
+  # set columns
   $cols = $seats.count
+  # set rows
   $rows = $seats[0].length
+  # create matching grid
   $grid = New-Object 'object[,]' $seats.count,$seats[0].length
+  # create object for 8 neighboars
   $neighbours = (-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)
   
+  # update grid to match input
   $counter1 = 0
   Foreach ($line in $seats) {
     $line = $line.ToCharArray()
@@ -114,13 +137,17 @@ Function Update-Seats2 {
     $counter1++
   }
 
+  # run through all seats and update output with rules. Want to keep it visual so setting output to lines
   for ($col = 0;$col -lt $cols;$col++) {
     for ($row = 0;$row -lt $rows;$row++) {
       Switch ($grid[$col,$row]) {
+        # no change, update line
         '.' {
           $showLine += '.'
         }
+        # based on empty chair rules
         'L' {
+          # read filled in sight
           $chairs = Foreach ($nb in $neighbours) {
             $counter = 1
             remove-variable done -erroraction silentlycontinue
@@ -139,15 +166,19 @@ Function Update-Seats2 {
               $counter++
             }
           }
+          # nothing in sight, fill it up
           If (!$chairs) {
             $showLine += '#'
             $seatsChanged = $true
           }
+          # keep same
           Else {
             $showLine += 'L'
           }
         }
+        # based on filled chair rules
         '#' {
+          # read filled in sight
           $chairs = Foreach ($nb in $neighbours) {
             $counter = 1
             remove-variable done -erroraction silentlycontinue
@@ -166,25 +197,34 @@ Function Update-Seats2 {
               $counter++
             }
           }
+          # above threshold, change to empty
           If ($chairs.count -ge 5) {
             $showLine += 'L'
             $seatsChanged = $true
           }
+          # below threshold, keep filled
           Else {
             $showLine += '#'
           }
         }
       }
     }
+    # add to multiline output
     $showAll += ,$showLine
+    # clean variable
     remove-variable showLine
   }
+  # show output if we changed
   If ($seatsChanged) {
     $showAll
   }
+  # no change, count number of filled chairs
   Else {
     (($showAll -join '').ToCharArray() | Where-Object {$_ -eq '#'}).count
   }
 }
+# initial from input
 $next = Update-Seats2 -seats (get-content .\puzzle11input.txt)
-While ($next -isnot [int]){$next = Update-Seats2 -seats $next};$next
+# loop through changes and report number of chairs
+While ($next -isnot [int]){$next = Update-Seats2 -seats $next}
+$next
