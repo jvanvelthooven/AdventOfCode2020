ea5cd39b4ddaa106c89ee40d5aef6a2e8c956b3e
Jeroen van Velthooven <jeroen.van.velthooven@vanderlande.com>
2020-12-15 23:08:19 +0100

    Puzzle 15 star 2

--- puzzle15solution.ps1
+++ puzzle15solution.ps1
@@ -31,8 +31,54 @@ $content = (get-content .\puzzle15input.txt).split(',') | ForEach-Object {[int]$
 # calc first step
 $next = Add-NextStep -array $content
 # run until 2020th entry
-While ($next.count -ne 2020){$next = Add-NextStep -array $next}
+While ($next.count -lt 2020){$next = Add-NextStep -array $next}
 # show last
 $next[-1]
 
 '2'
+Function Update-NextStep {
+  Param (
+    $data
+  )
+  # Get previous number
+  $prev = $data.last
+
+  # check if we already had this number
+  If (!$data.$prev -or $data.$prev -eq 0) {
+    # set next number to 0
+    $data.last = 0
+  }
+  Else {
+    # calculate next number
+    $data.last = $data.length - $data.$prev
+  }
+  # set location of this number
+  $data[$prev] = $data.length
+
+  # increase size
+  $data.length++
+}
+
+
+# Read content
+$content = (get-content .\puzzle15input.txt).split(',') | ForEach-Object {[int]$_}
+
+# create array for reuse
+$data = @{}
+
+# add last
+$data['last'] = $content[-1]
+
+# add length
+$data['length'] = $content.length
+
+# Add all content but last
+For ($step = 0;$step -lt ($content.count - 1);$step++) {
+  $data[$content[$step]] = $step + 1
+}
+
+# run until 2020th entry
+While ($data.length -lt 2020){Update-NextStep -data $data}
+
+# show last
+$data.last
