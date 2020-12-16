22799bba3e09772ad2bd620e132d8c2624fe53d6
Jeroen van Velthooven <jeroen.van.velthooven@vanderlande.com>
2020-12-16 21:39:16 +0100

    Puzzle 16 star 2

--- puzzle16solution.ps1
+++ puzzle16solution.ps1
@@ -1,5 +1,5 @@
 $limitations = get-content .\puzzle16input.txt | Where-Object {$_ -match ': '}
-$myTicket = (get-content .\puzzle16input.txt | Where-Object {$_ -match ','})[0]
+$myTicket = (get-content .\puzzle16input.txt | Where-Object {$_ -match ','})[0].split(',') | ForEach-Object {[int]$_}
 $tickets = (get-content .\puzzle16input.txt | Where-Object {$_ -match ','})[1..300]
 
 # get allowed numbers
@@ -22,3 +22,65 @@ $invalid = Foreach ($ticket in $tickets) {
 $invalid | Measure-Object -Sum
 
 '2'
+# remove invalid tickets and convert to arrays
+$validTickets = $tickets | Where-Object {
+  # convert to numbers in array
+  $ticket = $_.split(',') | ForEach-Object {[int]$_}
+
+  # check if ticket is valid
+  ($ticket | Where-Object {$allowedNumbers.contains($_)}).count -eq $ticket.count
+} | ForEach-Object {,$_.split(',')}
+
+# add fields to array of array
+$fields = Foreach ($limitation in $limitations) {
+  # name is first entry of the array
+  $name = ($limitation -split(': '))[0]
+  # the numbers is the 2nd
+  $numbers = $limitation | ForEach-Object {$split = $_.split(':')[1].split(' -');$split[1]..$split[2];$split[4]..$split[5]} | Sort-Object -Unique
+  # return the array for the collection of arrays
+  ,($name,$numbers)
+}
+
+# Loop until we found all three departures
+While ($dep.count -lt 6) {
+  # check all rows
+  For ($columnNr = 0;$columnNr -lt ($myTicket.length - 1);$columnNr++) {
+    # Check if we already have this row
+    If ($myTicket[$columnNr] -ne 'x') {
+      # get column
+      $column = $validTickets | ForEach-Object {$_[$columnNr]}
+
+      # match to field
+      $key = $fields | Where-Object {
+        $key = $_
+        ($column | Where-Object {$key[1].contains($_)}).count -eq $column.count
+      }
+
+      # Check if we got a single match
+      If ($key[0][0] -notmatch '[a-z]{2}') {
+        # remove from fields
+        $fields = $fields | Where-Object {$_[0] -ne $key[0]}
+        
+        # see if it starts with the given word
+        If ($key[0] -match '^departure') {
+          $dep += ,$columnNr
+        }
+
+        # mark as done
+        $myTicket[$columnNr] = 'x'
+
+        # break
+        break
+      }
+    }
+  }
+}
+
+# Rebuild my ticket as I broke it with my x's
+$myTicket = (get-content .\puzzle16input.txt | Where-Object {$_ -match ','})[0].split(',') | ForEach-Object {[int]$_}
+
+# sum
+$sum = 1;$dep | ForEach-Object {$sum *= $myTicket[$_]}
+
+# answer
+$sum
